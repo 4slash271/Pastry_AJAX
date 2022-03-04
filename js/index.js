e85255aadb96f8996d5f9398e489bc54042b9737
4slash271 <4slash271@gmail.com>
2022-03-04 23:44:28 +0900

    editing js for line circle

--- js/index.js
+++ js/index.js
@@ -1,6 +1,36 @@
 const mail = document.querySelector('.mail-wrapper .mail');
 const mailWrap= document.querySelector('.mail-wrapper');
 const mailStage= document.querySelector('.mail-stage');
+const lineTxt = document.querySelector('.line .line__text');
+const letter = 'Pastry AJAX KimSulki & KeemHeejoo';
+// arr = arr.join(',');
+
+let arr = letter.split('');//letter를 하나씩 분절
+for (var i = 0; i<arr.length-1; i++)
+arr.filter(function(i){ 
+        console.log(i);
+        // console.log(arr[i]);
+        
+             lineTxt.innerHTML = '<span class="circle">'+ i +'</span>';
+            lineTxt.innerHTML += '<span class="circle">'+ i +'</span>';
+            lineTxt.innerHTML += '<span class="circle">'+ i +'</span>';
+            lineTxt.innerHTML += '<span class="circle">'+ i +'</span>';
+            lineTxt.innerHTML += '<span class="circle">'+ i +'</span>';
+            lineTxt.innerHTML += '<span class="circle">'+ i +'</span>';
+      })
+// [].forEach.call(arr, function(i){ 
+//   for (var i = 0; i<arr.length-1; i++)
+//     console.log(i);
+//     console.log(arr[i]);
+    
+//     lineTxt.innerHTML = '<span class="circle">'+ arr[i] +'</span>';
+//         lineTxt.innerHTML += '<span class="circle">'+ arr[i] +'</span>';
+//         lineTxt.innerHTML += '<span class="circle">'+ arr[i] +'</span>';
+//         lineTxt.innerHTML += '<span class="circle">'+ arr[i] +'</span>';
+//         lineTxt.innerHTML += '<span class="circle">'+ arr[i] +'</span>';
+//         lineTxt.innerHTML += '<span class="circle">'+ arr[i] +'</span>';
+//   });
+
 
 
 function openMail(e){
