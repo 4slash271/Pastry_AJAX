9677f91931fb2daca711f2e3ff8dc59364c1c621
4slash271 <4slash271@gmail.com>
2022-03-05 23:52:03 +0900

    ...

--- js/index.js
+++ js/index.js
@@ -5,19 +5,22 @@ const lineTxt = document.querySelector('.line .line__text');
 const letter = 'Pastry AJAX KimSulki & KeemHeejoo';
 // arr = arr.join(',');
 
+
+
 let arr = letter.split('');//letter를 하나씩 분절
-for (var i = 0; i<arr.length-1; i++)
-arr.filter(function(i){ 
-        console.log(i);
-        // console.log(arr[i]);
+console.log(arr);
+//배열 안의 값을 반복문 안으로 가져온다
+//반복문 안에서 위의 배열값을 하나의 변수로 묶는다
+//묶은 변수는 하나씩 증가하며 변수는 배열값보다 하나 적고 0부터 시작한다
+//
+
+for(var i=0; i<arr.length-1; i++){
+    arr.forEach(function(i) {
         
-             lineTxt.innerHTML = '<span class="circle">'+ i +'</span>';
-            lineTxt.innerHTML += '<span class="circle">'+ i +'</span>';
-            lineTxt.innerHTML += '<span class="circle">'+ i +'</span>';
-            lineTxt.innerHTML += '<span class="circle">'+ i +'</span>';
-            lineTxt.innerHTML += '<span class="circle">'+ i +'</span>';
-            lineTxt.innerHTML += '<span class="circle">'+ i +'</span>';
-      })
+        '<span class="circle">'+ arr[i] +'</span>';
+
+      }
+    )
 // [].forEach.call(arr, function(i){ 
 //   for (var i = 0; i<arr.length-1; i++)
 //     console.log(i);
@@ -47,3 +50,4 @@ mail.addEventListener('click', openMail);
 mailWrap.addEventListener('mouseenter', stopRotate);
 mailWrap.addEventListener('mouseleave', startRotate);
 
+}
