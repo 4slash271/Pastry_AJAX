87a9721253768425c035388c551aa3b9c3ff9342
4slash271 <4slash271@gmail.com>
2022-03-06 00:30:57 +0900

    fixed line js and added css

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ const mail = document.querySelector('.mail-wrapper .mail');
 const mailWrap= document.querySelector('.mail-wrapper');
 const mailStage= document.querySelector('.mail-stage');
 const lineTxt = document.querySelector('.line .line__text');
-const letter = 'Pastry AJAX KimSulki & KeemHeejoo';
+const letter = 'Pastry~AJAX~KimSulki~&~KeemHeejoo';
 // arr = arr.join(',');
 
 
@@ -12,15 +12,16 @@ console.log(arr);
 //배열 안의 값을 반복문 안으로 가져온다
 //반복문 안에서 위의 배열값을 하나의 변수로 묶는다
 //묶은 변수는 하나씩 증가하며 변수는 배열값보다 하나 적고 0부터 시작한다
-//
+//변수는 함수 안에서 증가하면서 html 안에 적용된다
 
-for(var i=0; i<arr.length-1; i++){
-    arr.forEach(function(i) {
-        
-        '<span class="circle">'+ arr[i] +'</span>';
 
-      }
-    )
+
+        for(var i=0; i<arr.length-1; i++){
+
+        lineTxt.innerHTML += '<span class="circle">'+' '+ arr[i] +'</span>';
+    
+
+        }
 // [].forEach.call(arr, function(i){ 
 //   for (var i = 0; i<arr.length-1; i++)
 //     console.log(i);
@@ -50,4 +51,3 @@ mail.addEventListener('click', openMail);
 mailWrap.addEventListener('mouseenter', stopRotate);
 mailWrap.addEventListener('mouseleave', startRotate);
 
-}
