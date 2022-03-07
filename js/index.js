c76e7c74ccf62108341c0d6c9e03a986112e9f2a
4slash271 <4slash271@gmail.com>
2022-03-07 23:58:22 +0900

    ...

--- js/index.js
+++ js/index.js
@@ -3,39 +3,21 @@ const mailWrap= document.querySelector('.mail-wrapper');
 const mailStage= document.querySelector('.mail-stage');
 const lineTxt = document.querySelector('.line .line__text');
 const letter = 'Pastry~AJAX~KimSulki~&~KeemHeejoo';
-// arr = arr.join(',');
 
+const randomColors = "#57E653"
 
 
 let arr = letter.split('');//letter를 하나씩 분절
-console.log(arr);
-//배열 안의 값을 반복문 안으로 가져온다
-//반복문 안에서 위의 배열값을 하나의 변수로 묶는다
-//묶은 변수는 하나씩 증가하며 변수는 배열값보다 하나 적고 0부터 시작한다
-//변수는 함수 안에서 증가하면서 html 안에 적용된다
+// console.log(arr="p");
 
+// document.body.style.backgroundColor= "#" + Math.ceil(Math.random() * 0xffffff).toString(16);
+// body.document.style.backgroundColor = "#" + Math.ceil(Math.random() * 0xffffff).toString(16);
 
-
-        for(var i=0; i<arr.length-1; i++){
-
+for(var i=0; i<arr.length-1; i++){
         lineTxt.innerHTML += '<span class="circle">'+' '+ arr[i] +'</span>';
-    
-
         }
-// [].forEach.call(arr, function(i){ 
-//   for (var i = 0; i<arr.length-1; i++)
-//     console.log(i);
-//     console.log(arr[i]);
-    
-//     lineTxt.innerHTML = '<span class="circle">'+ arr[i] +'</span>';
-//         lineTxt.innerHTML += '<span class="circle">'+ arr[i] +'</span>';
-//         lineTxt.innerHTML += '<span class="circle">'+ arr[i] +'</span>';
-//         lineTxt.innerHTML += '<span class="circle">'+ arr[i] +'</span>';
-//         lineTxt.innerHTML += '<span class="circle">'+ arr[i] +'</span>';
-//         lineTxt.innerHTML += '<span class="circle">'+ arr[i] +'</span>';
-//   });
-
-
+let circle =document.querySelectorAll('.circle');
+console.log(circle);
 
 function openMail(e){
     window.location.href = "http://127.0.0.1:5500/html/mail.html"
